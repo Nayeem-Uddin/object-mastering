a9bfef380ce4aba991a36d84c37294b7605e1a5e
Nayeem-uddin <nayeemu02@gmail.com>
2021-02-08 11:02:36 +0600

    new.js

---
+++ new.js
@@ -0,0 +1,11 @@
+class person{
+    constructor(firstName,lastName,salary){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.salary = salary;
+    }
+}
+const person1 = new person('balam','khan',300);
+const person2 = new person('kulombo','mozumder',500);
+console.log(person1);
+console.log(person2);
